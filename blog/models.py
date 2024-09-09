67004e8d446c862e3913a3a8781db2af2445e929
Your Name <Your Name>
2024-09-09 13:35:15 +0000

    Testing

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+someth
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
